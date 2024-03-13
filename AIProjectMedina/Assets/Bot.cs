8aa9c08c5e1124a161aa5d1d9458f84779fe5ca5
SamuelM24 <beniciomedina11@gmail.com>
2024-03-13 13:47:20 -0700

    finished auto

--- AIProjectMedina/Assets/Bot.cs
+++ AIProjectMedina/Assets/Bot.cs
@@ -21,9 +21,11 @@ public class Bot : MonoBehaviour
         agent.SetDestination(location);
     }
 
+
     void Flee(Vector3 location)
     {
         Vector3 fleeVector = location - this.transform.position;
+
         agent.SetDestination(this.transform.position - fleeVector);
     }
 
@@ -44,6 +46,7 @@ public class Bot : MonoBehaviour
         float lookAhead = targetDir.magnitude / (agent.speed + ds.currentSpeed);
         Seek(target.transform.position + target.transform.forward * lookAhead);
     }
+
 
     void Evade()
     {
@@ -72,9 +75,121 @@ public class Bot : MonoBehaviour
         Seek(targetWorld);
     }
 
+    void Hide()
+    {
+        float dist = Mathf.Infinity;
+        Vector3 chosenSpot = Vector3.zero;
+
+        for (int i = 0; i < World.Instance.GetHidingSpots().Length; i++)
+        {
+
+            Vector3 hideDir = World.Instance.GetHidingSpots()[i].transform.position - target.transform.position;
+            Vector3 hidePos = World.Instance.GetHidingSpots()[i].transform.position + hideDir.normalized * 10;
+
+            if (Vector3.Distance(this.transform.position, hidePos) < dist)
+            {
+                chosenSpot = hidePos;
+                dist = Vector3.Distance(this.transform.position, hidePos);
+            }
+        }
+
+        Seek(chosenSpot);
+
+    }
+
+    void CleverHide()
+    {
+        float dist = Mathf.Infinity;
+        Vector3 chosenSpot = Vector3.zero;
+        Vector3 chosenDir = Vector3.zero;
+        GameObject chosenGO = World.Instance.GetHidingSpots()[0];
+
+        for (int i = 0; i < World.Instance.GetHidingSpots().Length; i++)
+        {
+            Vector3 hideDir = World.Instance.GetHidingSpots()[i].transform.position - target.transform.position;
+            Vector3 hidePos = World.Instance.GetHidingSpots()[i].transform.position + hideDir.normalized * 100;
+
+            if (Vector3.Distance(this.transform.position, hidePos) < dist)
+            {
+                chosenSpot = hidePos;
+                chosenDir = hideDir;
+                chosenGO = World.Instance.GetHidingSpots()[i];
+                dist = Vector3.Distance(this.transform.position, hidePos);
+            }
+        }
+
+        Collider hideCol = chosenGO.GetComponent<Collider>();
+
+        Ray backRay = new Ray(chosenSpot, -chosenDir.normalized);
+        RaycastHit info;
+        float distance = 250.0f;
+        hideCol.Raycast(backRay, out info, distance);
+
+        Seek(info.point + chosenDir.normalized);
+
+    }
+
+    bool CanSeeTarget()
+    {
+        RaycastHit raycastInfo;
+        
+        Vector3 rayToTarget = target.transform.position - this.transform.position;
+       
+        if (Physics.Raycast(this.transform.position, rayToTarget, out raycastInfo))
+        {
+            
+            if (raycastInfo.transform.gameObject.tag == "cop")
+                return true;
+        }
+        return false;
+    }
+
+ 
+    bool TargetCanSeeMe()
+    {
+        
+        Vector3 toAgent = this.transform.position - target.transform.position;
+        float lookingAngle = Vector3.Angle(target.transform.forward, toAgent);
+
+        
+        if (lookingAngle < 60)
+            return true;
+        return false;
+    }
+
+
+    bool coolDown = false;
+    void BehaviourCoolDown()
+    {
+        coolDown = false;
+    }
+
+    bool TargetInRange()
+    {
+        
+        if (Vector3.Distance(this.transform.position, target.transform.position) < 10)
+            return true;
+        return false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Wander();
+       
+        if (!coolDown)
+        {
+            if (!TargetInRange())
+            {
+                Wander();
+            }
+            else if (CanSeeTarget() && TargetCanSeeMe()) 
+            {                                             
+                CleverHide();                               
+                coolDown = true;
+                Invoke("BehaviourCoolDown", 5);             
+            }
+            else
+                Pursue();  
+        }
     }
 }
